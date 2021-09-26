26fcabf2df829617fc6667dd022ec90eca5140f0
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-09-26 20:48:29 +0300

    added files

---
+++ app.js
@@ -0,0 +1,30 @@
+let forwardBtn = document.querySelector('.forward')
+let backBtn = document.querySelector('.back')
+let imageContainer = document.querySelector('.image-container')
+let imgWidth = document.querySelector('img').clientWidth
+let totImages = document.querySelectorAll('img').length
+
+let sum = 0;
+
+let max = (-imgWidth) * (totImages - 2);
+
+forwardBtn.addEventListener('click', () => {
+    if (sum < max) {
+        imageContainer.style.transform = `translateX(0px)`;
+        sum = 0
+    } else {
+        imageContainer.style.transform = `translateX(${ sum - imgWidth }px)`;
+        sum -= imgWidth;
+    }
+})
+
+backBtn.addEventListener('click', () => {
+    if (sum === 0) {
+        imageContainer.style.transform = `translateX(${ max - imgWidth }px)`;
+        sum = max - imgWidth
+    } else {
+        imageContainer.style.transform = `translateX(${ sum + imgWidth }px)`;
+        sum += imgWidth
+    }
+})
+
